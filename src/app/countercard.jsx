36d59a1fd188bd7ec01a5e9d22fb4068c77ecaf3
Aditya Kothi <109725374+Adityagoudkothi@users.noreply.github.com>
2025-12-27 07:38:22 +0530

    added counter animation

--- src/app/countercard.jsx
+++ src/app/countercard.jsx
@@ -1,28 +1,46 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Card from "react-bootstrap/Card";
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 
-const CounterCard = ({ value, suffix, label }) => {
+const CounterCard = ({ value, suffix = "", label }) => {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, (latest) => Math.round(latest));
 
+  useEffect(() => {
+    const controls = animate(count, value, {
+      duration: 4,
+      ease: "easeOut",
+    });
+
+    return () => controls.stop();
+  }, [value]);
 
   return (
-    <Card
-      className="text-center border-0 h-100"
-      style={{
-        borderRadius: "18px",
-        background: "rgba(255,255,255,0.25)",
-        backdropFilter: "blur(10px)",
-        color: "#fff",
-        padding: "28px",
-        fontFamily:"Montserrat, sans-serif"
-      }}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 2, ease: "easeOut" }}
     >
-      <h2 className="fw-bold mb-1">
-        {value}
-        {suffix}
-      </h2>
-      <p className="mb-0 small">{label}</p>
-    </Card>
+      <Card
+        className="text-center border-0 h-100"
+        style={{
+          borderRadius: "18px",
+          background: "rgba(255,255,255,0.25)",
+          backdropFilter: "blur(10px)",
+          color: "#fff",
+          padding: "28px",
+          fontFamily: "Montserrat, sans-serif",
+        }}
+      >
+        <h2 className="fw-bold mb-1">
+          <motion.span>{rounded}</motion.span>
+          {suffix}
+        </h2>
+        <p className="mb-0 small">{label}</p>
+      </Card>
+    </motion.div>
   );
 };
 
